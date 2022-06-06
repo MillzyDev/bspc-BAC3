8580e94d257d0b0fed995a22719a5e31ad3c057d
Millzy <millzyyt08@gmail.com>
2022-06-06 22:20:09 +0100

    based

---
+++ BAC3/HarmonyPatches/AvatarTweenControllerPatch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAC3.HarmonyPatches
+{
+    internal class AvatarTweenControllerPatch
+    {
+    }
+}
---
+++ BAC3/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using IPA;
+using IPA.Config;
+using UnityEngine;
+using IPALogger = IPA.Logging.Logger;
+
+namespace BAC3
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Logger { get; private set; }
+
+        [Init]
+        public void Init(Config config, IPALogger logger)
+        {
+            Instance = this;
+            Logger = logger;
+            Logger.Info("BAC3 initialized.");
+        }
+    }
+}
