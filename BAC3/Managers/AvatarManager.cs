67f9749f674f1c581ac417734fe7c0553740e3fa
Millzy <millzyyt08@gmail.com>
2022-06-09 07:33:30 +0100

    make it better

--- BAC3/Managers/AvatarManager.cs
+++ BAC3/Managers/AvatarManager.cs
@@ -1,4 +1,5 @@
 ﻿using IPA.Utilities;
+using System.Linq;
 using UnityEngine;
 using Zenject;
 
@@ -29,40 +30,44 @@ namespace BAC3.Managers
             Plugin.Logger.Debug($"There are {trs.Length} AvatarDataModels");
 
             // match visuals
-            AvatarData data = trs[0].avatarData;
+            AvatarData data = trs.First((x) => x.avatarData != null).avatarData;
             AvatarPartsModel partsModel = ReflectionUtil.GetField<AvatarPartsModel, AvatarDataModel>(trs[0], "_avatarPartsModel");
-
-            AvatarMeshPartSO avatarMeshPartSO = partsModel.headTopCollection.GetById(data.headTopId) ?? partsModel.headTopCollection.GetDefault();
-            visualController.GetField<MeshFilter, AvatarVisualController>("_headTopMeshFilter").mesh = avatarMeshPartSO.mesh;
-            
-            AvatarMeshPartSO avatarMeshPartSO1 = partsModel.glassesCollection.GetById(data.glassesId) ?? partsModel.glassesCollection.GetDefault();
-            visualController.GetField<MeshFilter, AvatarVisualController>("_glassesMeshFilter").mesh = avatarMeshPartSO1.mesh;
-            
-            AvatarMeshPartSO avatarMeshPartSO2 = partsModel.facialHairCollection.GetById(data.facialHairId) ?? partsModel.facialHairCollection.GetDefault();
-            visualController.GetField<MeshFilter, AvatarVisualController>("_facialHairMeshFilter").mesh = avatarMeshPartSO2.mesh;
-            
-            AvatarMeshPartSO avatarMeshPartSO3 = partsModel.handsCollection.GetById(data.handsId) ?? partsModel.handsCollection.GetDefault();
-            visualController.GetField<MeshFilter, AvatarVisualController>("_leftHandsHairMeshFilter").mesh = avatarMeshPartSO3.mesh;
-            visualController.GetField<MeshFilter, AvatarVisualController>("_rightHandsHairMeshFilter").mesh = avatarMeshPartSO3.mesh;
-
-            AvatarMeshPartSO avatarMeshPartSO4 = partsModel.clothesCollection.GetById(data.clothesId) ?? partsModel.clothesCollection.GetDefault();
-            visualController.GetField<MeshFilter, AvatarVisualController>("_bodyMeshFilter").mesh = avatarMeshPartSO4.mesh;
-
-            AvatarSpritePartSO avatarSpritePartSO = partsModel.eyesCollection.GetById(data.eyesId) ?? partsModel.eyesCollection.GetDefault();
-            visualController.GetField<SpriteRenderer, AvatarVisualController>("_eyesSprite").sprite = avatarSpritePartSO.sprite;
-
-            AvatarSpritePartSO avatarSpritePartSO1 = partsModel.mouthCollection.GetById(data.eyesId) ?? partsModel.mouthCollection.GetDefault();
-            visualController.GetField<SpriteRenderer, AvatarVisualController>("_mouthSprite").sprite = avatarSpritePartSO1.sprite;
-
-            Plugin.Logger.Debug("Successfully set all avatar sprites and meshes!");
 
             Color color = partsModel.GetSkinColorById(data.skinColorId).Color;
             Color color1 = data.handsColor;
+
+            AvatarMeshPartSO avatarMeshPartSO = partsModel.headTopCollection.GetById(data.headTopId) ?? partsModel.headTopCollection.GetDefault();
+            MeshFilter headMeshFilter = visualController.GetField<MeshFilter, AvatarVisualController>("_headTopMeshFilter");
+            headMeshFilter.mesh = avatarMeshPartSO.mesh;
+            headMeshFilter.GetComponent<MeshRenderer>().material.color = color;
             
-            visualController.GetField<MulticolorAvatarPartPropertyBlockSetter, AvatarVisualController>("_headTopPropertyBlockColorSetter").SetColors(
-                data.headTopPrimaryColor,
-                data.headTopSecondaryColor
-                );
+            AvatarMeshPartSO avatarMeshPartSO1 = partsModel.glassesCollection.GetById(data.glassesId) ?? partsModel.glassesCollection.GetDefault();
+            MeshFilter glassesMeshFilter = visualController.GetField<MeshFilter, AvatarVisualController>("_glassesMeshFilter");
+            glassesMeshFilter.mesh = avatarMeshPartSO1.mesh;
+            
+            AvatarMeshPartSO avatarMeshPartSO2 = partsModel.facialHairCollection.GetById(data.facialHairId) ?? partsModel.facialHairCollection.GetDefault();
+            MeshFilter facialHairMeshFilter = visualController.GetField<MeshFilter, AvatarVisualController>("_facialHairMeshFilter");
+            facialHairMeshFilter.mesh = avatarMeshPartSO2.mesh;
+            
+            AvatarMeshPartSO avatarMeshPartSO3 = partsModel.handsCollection.GetById(data.handsId) ?? partsModel.handsCollection.GetDefault();
+            MeshFilter leftHandMeshFilter = visualController.GetField<MeshFilter, AvatarVisualController>("_leftHandsHairMeshFilter");
+            leftHandMeshFilter.mesh = avatarMeshPartSO3.mesh;
+            MeshFilter rightHandMeshFilter = visualController.GetField<MeshFilter, AvatarVisualController>("_rightHandsHairMeshFilter");
+            rightHandMeshFilter.mesh = avatarMeshPartSO2.mesh;
+
+            AvatarMeshPartSO avatarMeshPartSO4 = partsModel.clothesCollection.GetById(data.clothesId) ?? partsModel.clothesCollection.GetDefault();
+            MeshFilter bodyMeshFilter = visualController.GetField<MeshFilter, AvatarVisualController>("_bodyMeshFilter");
+            bodyMeshFilter.mesh = avatarMeshPartSO4.mesh;
+
+            AvatarSpritePartSO avatarSpritePartSO = partsModel.eyesCollection.GetById(data.eyesId) ?? partsModel.eyesCollection.GetDefault();
+            SpriteRenderer eyesRenderer = visualController.GetField<SpriteRenderer, AvatarVisualController>("_eyesSprite");
+            eyesRenderer.sprite = avatarSpritePartSO.sprite;
+
+            AvatarSpritePartSO avatarSpritePartSO1 = partsModel.mouthCollection.GetById(data.eyesId) ?? partsModel.mouthCollection.GetDefault();
+            SpriteRenderer mouthRenderer = visualController.GetField<SpriteRenderer, AvatarVisualController>("_mouthSprite");
+            mouthRenderer.sprite = avatarSpritePartSO1.sprite;
+
+            Plugin.Logger.Debug("Successfully set all avatar sprites and meshes!");
 
             visualController.GetField<MulticolorAvatarPartPropertyBlockSetter, AvatarVisualController>("_clothesPropertyBlockSetter").SetColors(
                 data.clothesPrimaryColor,
@@ -84,7 +89,8 @@ namespace BAC3.Managers
 
             visualController.GetField<AvatarPropertyBlockColorSetter, AvatarVisualController>("_facialHairPropertyBlockColorSetter").SetColor(color);
 
-            visualController.GetField<AvatarPropertyBlockColorSetter, AvatarVisualController>("_skinPropertyBlockColorSetter").SetColor(color);
+            visualController.GetField<AvatarPropertyBlockColorSetter, AvatarVisualController>("_skinPropertyBlockColorSetter")
+                .GetField<MaterialPropertyBlock, AvatarPropertyBlockColorSetter>("_materialPropertyBlock").SetColor(data.skinColorId, )
 
             Plugin.Logger.Debug("Successfully set all avatar colors!");
         }
